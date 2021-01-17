156650a106e822433b682f7fe887c0862454812e
harpal singh <harpal5142290786@gmail.com>
2021-01-16 19:44:19 -0500

    signup complete connect with firebase

--- app/src/main/java/com/example/carpooling/MainActivity.java
+++ app/src/main/java/com/example/carpooling/MainActivity.java
@@ -2,16 +2,43 @@ package com.example.carpooling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import static com.example.carpooling.R.id.button;
 
 public class MainActivity extends AppCompatActivity {
 private TextView tv;
+private Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) this.findViewById(R.id.textView);
         tv.setSelected(true);
+        b1=findViewById(button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Signup();
+            }
+        });
     }
+
+    private void Signup() {
+
+        Intent intent=new Intent(MainActivity.this,SignUpActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+
+
+
+
 }
