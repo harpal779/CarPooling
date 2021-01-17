1abd85cfe6f7401cf2ae40b15221ec822d3ce646
harpal singh <harpal5142290786@gmail.com>
2021-01-16 19:16:07 -0500

    marquee changes

--- app/src/main/java/com/example/carpooling/MainActivity.java
+++ app/src/main/java/com/example/carpooling/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.carpooling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) this.findViewById(R.id.textView);
+        tv.setSelected(true);
     }
 }
