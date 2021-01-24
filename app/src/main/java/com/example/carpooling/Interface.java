5dd707a4df2b5e28a4c29e2948cf6ca0b94275f4
harpal singh <harpal5142290786@gmail.com>
2021-01-24 18:02:48 -0500

    changes

--- app/src/main/java/com/example/carpooling/Interface.java
+++ app/src/main/java/com/example/carpooling/Interface.java
@@ -2,13 +2,26 @@ package com.example.carpooling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Interface extends AppCompatActivity {
-
+Button b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface);
+        b2=findViewById(R.id.button2);
+    }
+
+    public void Home(View view) {
+        Toast.makeText(Interface.this, "Logout Successfully", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(Interface.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
