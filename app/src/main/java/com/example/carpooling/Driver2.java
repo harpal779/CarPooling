6275ae2d750953d4f0c6c9a138b6fba08c320455
Avneet kaur <61361164+Avu2213@users.noreply.github.com>
2021-01-29 21:38:01 -0500

    driver and passenger activities

---
+++ app/src/main/java/com/example/carpooling/Driver2.java
@@ -0,0 +1,14 @@
+package com.example.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Driver2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver2);
+    }
+}
