2e8992beaaf58143adf869f70d4b32f5b4eca152
Avneet kaur <61361164+Avu2213@users.noreply.github.com>
2021-02-04 16:46:17 -0500

    changes

--- app/src/main/java/com/example/carpooling/Driver3.java
+++ app/src/main/java/com/example/carpooling/Driver3.java
@@ -2,13 +2,30 @@ package com.example.carpooling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Driver3 extends AppCompatActivity {
-
+Button h,l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver3);
+        h=findViewById(R.id.button13);
+        l=findViewById(R.id.button14);
+    }
+
+    public void Homee(View view) {
+        Intent intent=new Intent(Driver3.this,Driver1.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void Logoutt(View view) {
+        Intent intent=new Intent(Driver3.this,Login.class);
+        startActivity(intent);
+        finish();
     }
 }
