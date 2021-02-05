acb6dd96e86bd6ba00839124b241aad190c34386
Nand269 <65174601+nand269@users.noreply.github.com>
2021-02-04 19:57:16 -0500

    Update Passenger2.java

--- app/src/main/java/com/example/carpooling/Passenger2.java
+++ app/src/main/java/com/example/carpooling/Passenger2.java
@@ -1,14 +1,50 @@
 package com.example.carpooling;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Passenger2 extends AppCompatActivity {
-
+Button back,logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger2);
+        back=findViewById(R.id.button19);
+        logout=findViewById(R.id.button20);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back();
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+
+
+    }
+
+    private void logout() {
+        Intent intent = new Intent(Passenger2.this, Login.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void back() {
+
+        Intent intent = new Intent(Passenger2.this, Interface.class);
+        startActivity(intent);
+        finish();
+
     }
 }
