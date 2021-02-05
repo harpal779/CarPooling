36d4ab22057798f0621f079e50793a85ccb050f1
Sukhman23 <61361084+Sukhman23@users.noreply.github.com>
2021-02-04 20:32:05 -0500

    Update Driver2.java

--- app/src/main/java/com/example/carpooling/Driver2.java
+++ app/src/main/java/com/example/carpooling/Driver2.java
@@ -1,11 +1,11 @@
 package com.example.carpooling;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Driver2 extends AppCompatActivity {
 Button cancel;
@@ -14,9 +14,16 @@ Button cancel;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver2);
         cancel=findViewById(R.id.button10);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cancel();
+            }
+        });
+
     }
 
-    public void Cancel(View view) {
+    public void Cancel() {
         Intent intent=new Intent(Driver2.this,Driver1.class);
         startActivity(intent);
         finish();
