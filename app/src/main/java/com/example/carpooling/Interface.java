da3634dce87ba1de1f966159124a3203cc74e49d
harpal singh <harpal5142290786@gmail.com>
2021-02-04 19:51:02 -0500

    error fixes

--- app/src/main/java/com/example/carpooling/Interface.java
+++ app/src/main/java/com/example/carpooling/Interface.java
@@ -1,12 +1,12 @@
 package com.example.carpooling;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Interface extends AppCompatActivity {
 Button b2;
