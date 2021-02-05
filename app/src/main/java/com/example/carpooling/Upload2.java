6ffbe6864d1f3a2580a20125203ef0415b4115bf
Nand269 <65174601+nand269@users.noreply.github.com>
2021-02-05 00:05:51 -0500

    Upload2 method for post ride

---
+++ app/src/main/java/com/example/carpooling/Upload2.java
@@ -0,0 +1,84 @@
+package com.example.carpooling;
+
+public class Upload2 {
+
+    private String Carname;
+    private String Phone;
+    private String Model;
+    private String From;
+    private String To;
+    private String Date;
+    private String Time;
+    private String AvailableSeats;
+    private String Price;
+    
+    public Upload2(){}
+
+    public String getCarname() {
+        return Carname;
+    }
+
+    public void setCarname(String Carname) {
+        this.Carname = Carname;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    public String getFrom() {
+        return From;
+    }
+
+    public void setFrom(String From) {
+        this.From = From;
+    }
+    public String getTo() {
+        return To;
+    }
+
+    public void setTo(String To) {
+        this.To = To;
+    }
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String Time) {
+        this.Time = Time;
+    }
+    public String getAvailableSeats() {
+        return AvailableSeats;
+    }
+
+    public void setAvailableSeats(String AvailableSeats) {
+        this.AvailableSeats = AvailableSeats;
+    }
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String Price) {
+        this.Price = Price;
+    }
+}
