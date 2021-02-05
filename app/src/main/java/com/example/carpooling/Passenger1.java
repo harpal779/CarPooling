ba86d65bc51e475afcde404a0f430059f2406272
Nand269 <65174601+nand269@users.noreply.github.com>
2021-02-04 20:08:14 -0500

    Update Passenger1.java
    changes in passenger 1

--- app/src/main/java/com/example/carpooling/Passenger1.java
+++ app/src/main/java/com/example/carpooling/Passenger1.java
@@ -1,14 +1,58 @@
 package com.example.carpooling;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Passenger1 extends AppCompatActivity {
-
+Button home,back,logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger1);
+        home=findViewById(R.id.button18);
+        back=findViewById(R.id.button18);
+        logout=findViewById(R.id.button18);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back();
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home();
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+    }
+
+    private void logout() {
+        Intent intent = new Intent(Passenger1.this, Login.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void home() {
+        Intent intent = new Intent(Passenger1.this, Interface.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void back() {
+        Intent intent = new Intent(Passenger1.this, Passenger2.class);
+        startActivity(intent);
+        finish();
     }
 }
