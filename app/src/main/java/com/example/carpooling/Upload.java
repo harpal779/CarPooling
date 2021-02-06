c41c1b46184fe39df4c81809e321e8f7fe5e006b
Nand269 <65174601+nand269@users.noreply.github.com>
2021-02-05 23:46:29 -0500

    Update Upload.java

--- app/src/main/java/com/example/carpooling/Upload.java
+++ app/src/main/java/com/example/carpooling/Upload.java
@@ -2,33 +2,28 @@ package com.example.carpooling;
 
 public class Upload {
 
-    private String name;
-    private String gender;
-    private String age;
+    private String password;
+    private String email;
 
     public Upload(){}
 
-    public String getName() {
-        return name;
+    public String getPassword() {
+        return password;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getGender() {
-        return gender;
+
+    public String getEmail()
+    {
+        return email;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 
-    public String getAge() {
-        return age;
-    }
+    public void setEmail(String email) {
+        this.email=email;
 
-    public void setAge(String age) {
-        this.age = age;
     }
 }
