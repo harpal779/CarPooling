3ff96a707187f6b70ecc6ffa3c544dea38986cf3
Avneet kaur <61361164+Avu2213@users.noreply.github.com>
2021-02-05 20:18:58 -0500

    logout function working now

--- app/src/main/java/com/example/carpooling/Interface.java
+++ app/src/main/java/com/example/carpooling/Interface.java
@@ -9,19 +9,33 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Interface extends AppCompatActivity {
-Button b2,b21;
+Button b2,b21,b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface);
         b2=findViewById(R.id.button2);
         b21=findViewById(R.id.button21);
+        b4=findViewById(R.id.button4);
+
         b21.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 booked();
             }
         });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        Intent intent = new Intent(Interface.this, Login.class);
+        startActivity(intent);
+        finish();
     }
 
     private void booked() {
