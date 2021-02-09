796ddf39d63e6c48ac0bd26c903a68717d106bf9
Nand269 <65174601+nand269@users.noreply.github.com>
2021-02-08 22:48:35 -0500

    Passenger 2 class update list of available rides
    Passenger 2 class update list of available rides

--- app/src/main/java/com/example/carpooling/Passenger2.java
+++ app/src/main/java/com/example/carpooling/Passenger2.java
@@ -2,19 +2,43 @@ package com.example.carpooling;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Passenger2 extends AppCompatActivity {
 Button back,logout;
+    RecyclerView recyclerView1;
+    FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databasereference;
+    private FirebaseAuth firebaseAuth;
+    private String auth,name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger2);
         back=findViewById(R.id.button19);
         logout=findViewById(R.id.button20);
+        firebaseAuth = FirebaseAuth.getInstance();
+        recyclerView1 = findViewById(R.id.recyclerView2);
+        recyclerView1.setHasFixedSize(true);
+        recyclerView1.setLayoutManager(new LinearLayoutManager(this));
+        auth=firebaseAuth.getCurrentUser().getUid();
+        databasereference = firebaseDatabase.getInstance().getReference().child("Ride");
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,4 +71,44 @@ Button back,logout;
         finish();
 
     }
-}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Upload2> options1 =
+                new FirebaseRecyclerOptions.Builder<Upload2>()
+                        .setQuery(databasereference,Upload2.class)
+                        .build();
+
+
+        FirebaseRecyclerAdapter<Upload2,ViewHolder2> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Upload2, ViewHolder2>(options1) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ViewHolder2 holder, int position, @NonNull Upload2 model) {
+                        holder.setData(getApplicationContext(),model.getCarname(),model.getPhone(),model.getModel(),model.getFrom(),model.getTo(),model.getDate(),model.getTime(),model.getAvailableSeats(),model.getPrice());
+                        holder.setOnClickListener(new ViewHolder.Clicklistener() {
+                            @Override
+                            public void onItemlongClick(View view, int position) {
+                                name = getItem(position).getPhone();
+
+                            }
+                        });
+                    }
+                    @NonNull
+                    @Override
+                    public ViewHolder2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View view = LayoutInflater.from(parent.getContext())
+                                .inflate(R.layout.row,parent,false);
+
+                        return new ViewHolder2(view);
+                    }
+                };
+
+        firebaseRecyclerAdapter.startListening();
+        recyclerView1.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+                }
